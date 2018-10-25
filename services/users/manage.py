9382ac7d443b6eeb4e6490ac0bb1baa1382232f4
dsdunn <dsdunnmusic@gmail.com>
2018-10-25 14:57:19 -0600

    Worked through docker config, postgres and test setup

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,9 +1,27 @@
 from flask.cli import FlaskGroup
 
-from project import app
+from project import app, db  
+
+import unittest
+
 
 cli = FlaskGroup(app)
 
-if __name__ == '__main__':
-  cli()
 
+@cli.command()
+
+def test():
+  """Runs the tests without code coverage"""
+  tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+  result = unittest.TextTestRunner(verbosity=2).run(tests)
+  if result.wasSuccessful():
+    return 0
+  return 1
+
+# def recreate_db():
+#     db.drop_all()
+#     db.create_all()
+#     db.session.commit()
+
+if __name__ == '__main__':
+    cli()
